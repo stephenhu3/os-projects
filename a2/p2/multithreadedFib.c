bd654c15c120ea526d19283e7f6976db10b020b2
stephenhu3 <stephenhu3@gmail.com>
2015-01-26 18:47:56 -0800

    first commit of multithreaded program

--- a2/p2/multithreadedFib.c
+++ a2/p2/multithreadedFib.c
@@ -1,26 +1,48 @@
 #include <windows.h>
 #include <stdio.h>
-DWORD Sum; /* data is shared by the thread(s) */
+DWORD fibSum; /* data is shared by the thread(s) */
 
 /* the thread runs in this separate function */ 
-DWORD WINAPI Summation(LPVOID Param) {
-   DWORD Upper = *(DWORD*)Param;
-   for (DWORD i = 0; i <= Upper; i++)
-     Sum += i;
-   return 0;
+DWORD WINAPI calculateFib(LPVOID nthFib) {
+   DWORD n = *(DWORD*)nthFib;
+
+   //implement fibonacci calculation iteratively
+   DWORD sum1 = 0;
+   DWORD sum2 = 1;
+   DWORD preFibSum;
+     
+   if (n == 0) {
+    preFibSum = 0;
+   } else if (n == 1) {
+    preFibSum = 1;
+   } else {
+      DWORD temp;
+
+    for (DWORD i = 2; i <= n; i++) {
+      temp = sum2;
+      sum2 = sum1 + sum2; 
+      sum1 = temp;
+    }
+    preFibSum = sum2;
+  }
+
+  fibSum = preFibSum;
+
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
   DWORD ThreadId;
   HANDLE ThreadHandle;
-  int Param;
+  int nthFib;
 
   if (argc != 2) {
-    fprintf(stderr,"An integer parameter is required\n"); 
+    fprintf(stderr,"A single integer parameter is required\n"); 
     return -1;
   }
-  Param = atoi(argv[1]); 
-  if (Param < 0) {
+  nthFib = atoi(argv[1]);
+
+  if (nthFib < 0) {
     fprintf(stderr,"An integer >= 0 is required\n");
     return -1;
   }
@@ -29,17 +51,18 @@ int main(int argc, char *argv[]) {
    ThreadHandle = CreateThread(
      NULL, /* default security attributes */
      0, /* default stack size */
-     Summation, /* thread function */
-     &Param, /* parameter to thread function */
+     calculateFib, /* thread function */
+     &nthFib, /* parameter to thread function */
      0, /* default creation flags */
      &ThreadId); /* returns the thread identifier */
 
   if (ThreadHandle != NULL) {
-    /* now wait for the thread to finish */
-    WaitForSingleObject(ThreadHandle,INFINITE);
-    
-    /* close the thread handle */
-    CloseHandle(ThreadHandle);
+      /* now wait for the thread to finish */
+      WaitForSingleObject(ThreadHandle,INFINITE);
+      
+      /* close the thread handle */
+      CloseHandle(ThreadHandle);
 
-    printf("sum = %d\n",Sum); }
-  }
+      printf("The %d fibonacci number is %d\n",fibSum); 
+    }
+}
