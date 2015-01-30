09f80de5cd86f674f185435e8f563f78f66c768f
stephenhu3 <stephenhu3@gmail.com>
2015-01-29 23:00:31 -0800

    removing old files

--- Activity3_RW/.simple.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/stephen/G20_Repo/Activity3_RW/simple.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.8.0-44-generic/scripts/module-common.lds --build-id  -o /home/stephen/G20_Repo/Activity3_RW/simple.ko /home/stephen/G20_Repo/Activity3_RW/simple.o /home/stephen/G20_Repo/Activity3_RW/simple.mod.o
--- Activity3_RW/.tmp_versions/simple.mod
+++
@@ -1,2 +0,0 @@
-/home/stephen/G20_Repo/Activity3_RW/simple.ko
-/home/stephen/G20_Repo/Activity3_RW/simple.o
