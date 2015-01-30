0ab907e7dd8b28053fe33d1ea0b4e6d6680ad991
stephen group 20 <stephenhu3@gmail.com>
2015-01-30 13:18:00 -0800

    cleaned and formatted

--- a2/p2/minishell.c
+++ a2/p2/minishell.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
 		
 		parsePath(*directories);
 		/* Get the full pathname for the file */
-		command.name = lookupPath(command.argv, *directories); //thisone
+		command.name = lookupPath(command.argv, *directories);
 		printf(command.name);
 		if(command.name == NULL) {
 			/* Report error */ 
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
 			continue;
 		}
 
-		 Create child process and execute the command  
+		 // Create child process and execute the command  
 		if((pid = fork()) == 0) {
 			/* Child executing command */ 
 			
@@ -55,9 +55,13 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-/* Determine command name and construct the parameter list. This function will build argv[] and set the argc value.
-argc is the number of "tokens" or words on the command line and argv[] is an array of strings (pointers to char *).
-The last element in argv[] must be NULL. As we scan the command line from the left, the first token goes in argv[0], the second in argv[l], and so on. Each time we add a token to argv[], we increment argc.
+/* Determine command name and construct the parameter list. 
+This function will build argv[] and set the argc value.
+argc is the number of "tokens" or words on the command line and argv[] 
+is an array of strings (pointers to char *). The last element in argv[]
+must be NULL. As we scan the command line from the left, the first token 
+goes in argv[0], the second in argv[l], and so on. Each time we add a token 
+to argv[], we increment argc.
 */
 int parseCommand(char *cLine, struct command_t *cmd) { 
 	int argc;
@@ -80,7 +84,8 @@ int parseCommand(char *cLine, struct command_t *cmd) {
 }
 
 void printPrompt() {
-	/* Build the prompt string to have the machine name, current directory, or other desired information */
+	/* Build the prompt string to have the machine name, current directory, 
+	or other desired information */
     char promptString[CHAR_MAX];
     strcpy(promptString, "");
     char hostname[CHAR_MAX];
@@ -101,12 +106,15 @@ void printPrompt() {
 }
 
 void readCommand(char *buffer) {
-	/* This code uses any set of I/O functions, such as those in the stdio library to read the entire command line into the buffer. This implementation is greatly simplified, but it does the job. */
+	/* This code uses any set of I/O functions, such as those in the 
+	stdio library to read the entire command line into the buffer. 
+	This implementation is greatly simplified, but it does the job. */
 	gets (buffer);
 }
 
 int parsePath(char **dirs) {
-	/* This function reads the PATH variable for this environment, then builds an array, dirs[], of the directories in PATH */
+	/* This function reads the PATH variable for this environment, 
+	then builds an array, dirs[], of the directories in PATH */
 	char *pathEnvVar; 
 	char *thePath;
 
@@ -115,7 +123,8 @@ int parsePath(char **dirs) {
         int i;
 	
 	for(i=0; i<MAX_PATHS; i++) 
-		dirs[i] = NULL;  set to null 
+		dirs[i] = NULL;  //set to null 
+	
 	pathEnvVar = (char *) getenv ("PATH");
 
 
@@ -123,7 +132,8 @@ int parsePath(char **dirs) {
 	thePath = (char *) malloc(strlen(pathEnvVar) + 1);
 	strcpy(thePath, pathEnvVar);
 	
-	// /* Loop to parse thePath. Look for a ':' delimiter between each path name. */
+	/* Loop to parse thePath. 
+	Look for a ':' delimiter between each path name. */
 	for(i=0; (dirs[i] = strsep(thePath, ":")) != NULL; i++) {
                 dirs[++i] = (char *) malloc(MAX_ARG_LEN);
     }
@@ -132,8 +142,10 @@ int parsePath(char **dirs) {
 }
 
 char *lookupPath(char **argv, char **dir) {
-	/* This function searches the directories identified by the dir argument to see if argv[0] (the file name) appears there.
-	Allocate a new string, place the full path name in it, then return the string. */
+	/* This function searches the directories identified by 
+	the dir argument to see if argv[0] (the file name) appears there.
+	Allocate a new string, place the full path name in it, 
+	then return the string. */
 	int i;
 	char *result;
 	char pName[MAX_PATH_LEN];
@@ -151,7 +163,7 @@ char *lookupPath(char **argv, char **dir) {
 	/* Look in PATH directories
 	   use access() to see if the file is in a dir */
 	for (i = 0; i < pathSize; i++) {
-		// strcat(filepath, dir[i]); // this line is causing crashes
+		strcat(filepath, dir[i]);
 		strcat(filepath, argv[0]);
 		printf(dir[i]);
 
