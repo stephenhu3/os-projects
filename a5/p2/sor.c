b9d26fabb9dd0ceaf6b82eccf078541e0889fefd
stephen group 20 <stephenhu3@gmail.com>
2015-03-14 08:28:02 -0700

    single process version of successive over relaxation program to solve n x n linear system complete. need to implement multiple process version with shared memory

--- a5/p2/sor.c
+++ a5/p2/sor.c
@@ -3,8 +3,13 @@
 #include <stdlib.h>
 #include <assert.h>
 
+
+
 // n symbolic constant between 3 to 6
 #define n 3
+#define MAX_FLOAT 100000
+#define MIN_FLOAT 0
+
 // may use shmget, shmat, shmdt, shmctl,...
 // may use shm_open, mmap, shm_unlink,...
 
@@ -24,18 +29,106 @@
 // include A and b as arrays inside program
 // output to stdout
 
-// inputs: A, b, omega, sigma, phi
+// inputs: A, b, omega
+// outputs: phi (solutions to n variables)
 
+float * solveSystem(float A[n][n], float b[n]);
 
-int sigma;
-int omega = 2;// set to greatet than 1 for speeding up convergency of a slow-converging process, while values less than 1 are used to help establish convergence of a diverging iterative process or speed up convergence of an overshooting process
-int i;
-int j;
-for (i = 1; i <= n; i++) {
-	sigma = 0;
-	for (j = 1; j <= n; j++) {
-		if (j != i)
-			sigma = sigma + (A[i][j] * phi[j]);
+int main() {
+	float A[3][3] = { { 12.0, 3.0, -5.0 }, { 1.0, 5.0, 3.0 }, { 3.0, 7.0, 13.0 } };
+	float b[3] = { 1, 28, 76 };
+
+	int i;
+	for (i = 0; i < n; i++) {
+		printf("%f \n", *(solveSystem(A, b)+i));
 	}
-	phi[i] = (1-omega)*phi[i] + (omega/A[i][i])*(b[i]-sigma);
+
+	return 0;
+}
+
+float * solveSystem(float A[n][n], float b[n]) {
+
+	float error[n];
+
+	float maxError = MIN_FLOAT;
+	float currentError = MAX_FLOAT;
+
+	/* relaxation factor must be 0 < omega < 2
+	   check convergence: the new value is same as previous value
+	   accomplish this by checking for approximate error between 
+	   new value and old value, the max should be close to 0 */
+
+	static float phi[n];
+	int i;
+	for (i = 0; i < n; i++)
+		phi[i] = 0; // set initial guess to zeroes
+
+	float oldphi[n];
+	for (i = 0; i < n; i++)
+		oldphi[i] = 0; // set initial guess to zeroes
+	// Alternatively, we can clear to zeroes using calloc
+
+	while (currentError > 0.0001) {
+		
+
+		float sigma;
+		/* set to greater than 1 for speeding up convergency of a 
+		slow-converging process, while values less than 1 are used to help 
+		establish convergence of a diverging iterative process or speed up 
+		convergence of an overshooting process */
+		float omega = 1.75;
+		int j;
+
+		int k;
+		for (k = 0; k < n; k++)
+				oldphi[k] = phi[k]; // old phi has previous values
+
+		for (i = 0; i < n; i++) {
+			sigma = 0;
+			for (j = 0; j < n; j++) {
+				if (j != i)
+					sigma = sigma + (A[i][j] * phi[j]);
+			}
+			
+			phi[i] = (1-omega)*phi[i] + (omega / A[i][i]) * (b[i]-sigma);
+
+		}
+
+		/* for debugging purposes
+		printf("oldphi: \n");
+		for (k = 0; k < n; k++) {
+			printf("%f \n", oldphi[k]); // old phi has previous values
+		}
+
+		printf("phi: \n");
+		for (k = 0; k < n; k++) {
+			printf("%f \n", phi[k]); // new calculated phi values
+		} */
+		
+
+		//calculate errors
+		int m;
+		for (m = 0; m < n; m++) {
+			error[m] = ((phi[m]-oldphi[m])/(phi[m])) * 100;
+		}
+		
+		/* for debugging purposes
+		printf("errors: \n");
+		for (k = 0; k < n; k++) {
+			printf("%f \n", error[k]); // new calculated phi values
+		} */
+
+
+		// check for max error
+		maxError = MIN_FLOAT;
+
+		for (m = 0; m < n; m++) {
+			if (error[m] > maxError)
+				maxError = error[m];
+		}
+
+		currentError = maxError;
+		
+	}
+	return phi;
 }
