3be6b596f579d43e9c91388c32a86e16e3a3eaef
Rico group 20 <rico_wen@hotmail.com>
2015-03-15 18:06:09 -0700

    initial skeleton commit

---
+++ a5/p1/vmm.c
@@ -0,0 +1,7 @@
+#include "vmm.h"
+
+int main(int argc, char **argv) {
+	int addresses[NUMTESTADDR] = {1,256,32768,128,65534,33153};
+
+
+}
---
+++ a5/p1/vmm.h
@@ -0,0 +1,19 @@
+#ifndef VMM_H
+#define VMM_H
+
+// INCLUDES
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// DEFINES
+
+#define NUMTESTADDR 6
+
+// STRUCT DEFINITIONSS
+
+// FUNCTION PROTOTYPES
+
+
+#endif
