6ea72d32686ccf86be283857820d411289ba118a
Rico group 20 <rico_wen@hotmail.com>
2015-03-15 19:03:06 -0700

    now reads 128 addresses from addresses.txt

--- a5/p1/vmm.c
+++ a5/p1/vmm.c
@@ -1,18 +1,28 @@
 #include "vmm.h"
 
 int main(int argc, char **argv) {
-	int addresses[NUMTESTADDR] = {1,256,32768,128,65534,33153};
+	// int addresses[NUMTESTADDR] = {1,256,32768,128,65534,33153};
 
 	int addrCount;
+	
+	// time to read from addresses.txt
+	int addrRead;
+	int *addresses = (int *) malloc(sizeof(int *) * MAXADDR);
 
-	for(addrCount = 0; addrCount < NUMTESTADDR; addrCount++) {
+	getAddr(argv[1], &addrRead, addresses);
+
+	printf("%i\n", addrRead);
+
+	for(addrCount = 0; addrCount < addrRead; addrCount++) {
 		int pageNum = getPageNum(addresses[addrCount]);
 		int pageOffset = getPageOffset(addresses[addrCount]);
 
 		printf("Logical Address: %i\n", addresses[addrCount]);
-		printf("Page Number: %i\n", pageNum); 
+		printf("Page Number: %i\n", pageNum);
 		printf("Page Offset: %i\n", pageOffset);
 	}
+
+	free(addresses);
 }
 
 //PARAMS: logical address
@@ -27,4 +37,20 @@ int getPageNum(int address) {
 //RETURNS: address masked to 8 bits (bits 0-7 = page offset)
 int getPageOffset(int address) {
 	return (address & 0xFF);
+}
+
+//PARAMS: filename (addresses.txt) and address read counter
+//EFFECTS: reads addresses from file to address array while incrementing counter
+//RETURNS: none
+void getAddr(char *fileName, int *addrRead, int *addresses) {
+	int i;
+
+	FILE *f = fopen(fileName, "r");
+
+	for(i = 0; i < MAXADDR; i++) {
+		if(fscanf(f, "%i", &addresses[i]) != 1) {
+			break;
+		}
+		*addrRead = i;
+	}
 }
