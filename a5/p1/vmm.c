fc07d4b1e3699ae3409aafaa1c31cc9e4007b062
Rico group 20 <rico_wen@hotmail.com>
2015-03-20 01:03:19 -0700

    physical address working but the value is giving me a seg fault :( going to see what's up

--- a5/p1/vmm.c
+++ a5/p1/vmm.c
@@ -21,14 +21,6 @@ int main(int argc, char **argv) {
 
 	// TODO: implement checking for arguments (ie. ./vmm filename.txt derperperpdp)
 
-	// Read from addresses.txt
-	int *addresses = (int *) malloc(sizeof(int *) * MAXADDR);
-	getAddr(argv[1], &addrRead, addresses);
-	printf("%i\n", addrRead);
-
-	// Initializes and clears page table
-	initPageTable(&pageTable);
-
 	// Open BACKING_STORE.bin into a file
 	backStore = NULL;
 	backStore = fopen("BACKING_STORE.bin", "r");
@@ -40,24 +32,52 @@ int main(int argc, char **argv) {
 	else
 		printf("BACKING_STORE.bin is opened\n");
 
+	// Initializes and clears page table
+	initPageTable(&pageTable);
+
+	// Initializes and clears TLB
+	initTLB(&tlb);
+
+	// Read from addresses.txt
+	int *addresses = (int *) malloc(sizeof(int *) * MAXADDR);
+	getAddr("addresses.txt", &addrRead, addresses);
+	printf("%i\n", addrRead);
+
 	for(addrCount = 0; addrCount <= addrRead; ++addrCount) {
 		int pageNum = getPageNum(addresses[addrCount]);
 		int pageOffset = getPageOffset(addresses[addrCount]);
 
-		frameNum = findPageNum(&pageTable, pageNum);
+		int frameNum = checkTLB(&tlb, pageNum);
+		
+		if(frameNum == -1)
+			frameNum = findPageNum(&pageTable, pageNum);
+		else
+			tlbHits++;
 
-		if(!frameNum)
+		if(frameNum != -1)
 			page = pageTable.page[frameNum];
 		else {
 			page = readPage(backStore, pageNum);
+			frameNum = backStoreUpdate(&pageTable, page, pageNum);
 			pageFaults++;
 		}
 		
-		printf("Logical Address: %i  ", addresses[addrCount]);
-		printf("Physical Address: %i\n", (frameNum * 0x100) + pageOffset);
-		// printf("Page Number: %i\n", pageNum);
+		// printf("Frame Number: %i  ", frameNum);
+		printf("Virtual Address: %i  ", addresses[addrCount]);
+		printf("Physical Address: %i  ", (frameNum * 0x100) + pageOffset);
+		// printf("Value: %i\n", page[pageOffset]);
+		// printf("Page Number: %i  ", pageNum);
 		// printf("Page Offset: %i\n", pageOffset);
+
+		updateTLB(&tlb, pageNum, frameNum);
 	}
+
+	printf("Number of Translated Addresses = %i\n", ++addrRead);
+	printf("Page Faults = %i\n", pageFaults);
+	printf("Page Fault Rate = %f\n", (double) pageFaults / (double) (++addrRead));
+
+	printf("TLB Hits = %i\n", tlbHits);
+	printf("TLB Hit Rate = %f\n", (double) tlbHits / (double) (++addrRead));
 
 	free(addresses);
 
@@ -99,7 +119,6 @@ void getAddr(char *fileName, int *addrRead, int *addresses) {
 // EFFECTS: To initialize each index with -1 and 0 for the number of page and number of frame relatively. And to set frame count to 0
 // RETURNS: none
 void initPageTable(struct page_table *pagetable) {
-
     int i;
     for (i = 0; i < FRAME_SIZE; i++) {
         pagetable->page_num[i] = -1;
@@ -112,7 +131,6 @@ void initPageTable(struct page_table *pagetable) {
 // MODIFIES: none
 // EFFECTS: seek to byte position offset * PAGE_SIZE, and returns the buffer.
 char *readPage(FILE *backStore, int offset){
-
     char *buffer = malloc(PAGE_SIZE);
 
     fseek(backStore, offset * PAGE_SIZE, SEEK_SET);
@@ -130,6 +148,92 @@ int findPageNum(struct page_table *pageTable, int pageNum) {
 		if(pageTable->page_num[i] == pageNum)
 			return i;
 	}
+
 	return -1;
+}
+
+//PARAMS: page table and page number (0 to 255)
+//EFFECTS: updates page table with new page from BACKING_STORE.bin
+//RETURNS: none
+int backStoreUpdate(struct page_table *pageTable, char *page, int pageNum) {
+	int i, frameNum, limit, freeIndex;
+
+	if(pageTable->frame_count == FRAME_SIZE) {
+		for(i = 0; i < FRAME_SIZE; i++) {
+			if(pageTable->counter[i] >= limit) {
+				limit = pageTable->counter[i];
+				freeIndex = i;
+			}
+		}
+	pageTable->page[freeIndex] = page;
+	pageTable->page_num[freeIndex] = pageNum;
+	pageTable->counter[freeIndex] = 0;
+	frameNum = freeIndex;
+	}
+	else {
+		pageTable->page[pageTable->frame_count] = page;
+		pageTable->page_num[pageTable->frame_count] = pageNum;
+		frameNum = pageTable->frame_count;
+		pageTable->frame_count++;
+	}
+
+	for(i = 0; i < pageTable->frame_count; i++)
+		pageTable->counter[i]++;
+
+	return frameNum;
+}
+
+//PARAMS: tlb struct
+//EFFECTS: initialize pageNum, frameNum values to -1 and counter values and tlbcount to 0
+//RETURNS: none
+void initTLB(struct tlb *tlb) {
+	int i;
+
+	for(i = 0; i < TLB_SIZE; i++) {
+		tlb->pageNum[i] = -1;
+		tlb->frameNum[i] = -1;
+		tlb->counter[i] = 0;
+	}
+	tlb->tlbCount = 0;
+}
+
+//PARAMS: tlb struct and page number
+//EFFECTS: checks if tlb contains frameNum for pageNum
+//RETURNS: returns frameNum or -1 on failure
+int checkTLB(struct tlb *tlb, int pageNum) {
+	int i;
+
+	for(i = 0; i < tlb->tlbCount; i++)
+		if(tlb->pageNum[i] == pageNum)
+			return tlb->frameNum[i];
+
+	return -1;
+}
+
+//PARAMS: tlb struct, page number, and frame number
+//EFFECTS: updates tlb with page and frame number eviction where necessary
+//RETURNS: none
+void updateTLB(struct tlb *tlb, int pageNum, int frameNum) {
+	int i, limit, freeIndex;
+
+	if(tlb->tlbCount == TLB_SIZE) {
+		for(i = 0; i < TLB_SIZE; i++) {
+			if(tlb->counter[i] >= limit) {
+				limit = tlb->counter[i];
+				freeIndex = i;
+			}
+		}
+		tlb->pageNum[freeIndex] = pageNum;
+		tlb->frameNum[freeIndex] = frameNum;
+		tlb->counter[i] = 0;
+	}
+	else {
+		tlb->pageNum[tlb->tlbCount] = pageNum;
+		tlb->frameNum[tlb->tlbCount] = frameNum;
+		tlb->tlbCount++;
+	}
+
+	for(i = 0; i < tlb->tlbCount; i++)
+		tlb->counter[i]++;
 }
 
--- a5/p1/vmm.h
+++ a5/p1/vmm.h
@@ -11,7 +11,7 @@
 // DEFINES
 
 #define NUMTESTADDR 6
-#define MAXADDR 1024
+#define MAXADDR 4096
 
 #define PAGE_SIZE 256
 #define FRAME_SIZE 128
@@ -41,6 +41,10 @@ void getAddr(char *fileName, int *addrRead, int *addresses);
 void initPageTable(struct page_table *pagetable);
 char *readPage(FILE *backStore, int offset);
 int findPageNum(struct page_table *pageTable, int pageNum);
+int backStoreUpdate(struct page_table *pageTable, char *page, int pageNum);
 
+void initTLB(struct tlb *tlb);
+int checkTLB(struct tlb *tlb, int pageNum);
+void updateTLB(struct tlb *tlb, int pageNum, int frameNum);
 
 #endif
