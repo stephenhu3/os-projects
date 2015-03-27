748ecefed1a96334f75fa94f0a29593f2b80f751
Rico group 20 <rico_wen@hotmail.com>
2015-03-27 12:21:57 -0700

    initial skeleton

---
+++ a6/hostds.h
@@ -0,0 +1,17 @@
+#ifndef HOSTDS
+#define HOSTDS
+
+// INCLUDES
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+// DEFINES
+
+// STRUCT DEFINITIONSS
+
+// FUNCTION PROTOTYPES
+
+#endif
