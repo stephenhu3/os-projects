390c83e13811b8027d6d1673107afb069ec52c2f
Rico group 20 <rico_wen@hotmail.com>
2015-03-30 18:23:22 -0700

    implemented host and intialization, queues and processes TODO

--- a6/p1/hostds.c
+++ a6/p1/hostds.c
@@ -1,5 +1,34 @@
 #include "hostds.h"
 
-int main(int argc, char **argv) {
+struct host host;
 
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		printf("Error processing arguments... Please specify filename in format: ./hostds filename.txt\n");
+		exit(0);
+	}
+
+	initSys();
+
+	// printf("Printers: %i\n", host.numPrinters);
+}
+
+void initSys(void) {
+	// initialize constants
+	host.numPrinters = NUM_PRINTERS;
+	host.numScanners = NUM_SCANNERS;
+	host.numModems = NUM_MODEMS;
+	host.numDrives = NUM_DRIVES;
+	host.totalMem = TOTAL_MEM;
+	host.rtMemory = TOTAL_RTMEM;
+	host.rtMemoryUsed = 0;
+	
+	// sets array members to 0
+	memset(host.printers, 0, NUM_PRINTERS);
+	memset(host.scanners, 0, NUM_SCANNERS);
+	memset(host.modems, 0, NUM_MODEMS);
+	memset(host.drives, 0, NUM_DRIVES);
+	memset(host.memory, 0, TOTAL_MEM);
+
+	// TODO -- QUEUES
 }
--- a6/p1/hostds.h
+++ a6/p1/hostds.h
@@ -10,8 +10,41 @@
 
 // DEFINES
 
-// STRUCT DEFINITIONSS
+#define NUM_PRINTERS 2
+#define NUM_SCANNERS 1
+#define NUM_MODEMS 1
+#define NUM_DRIVES 2
+#define TOTAL_MEM 1024
+#define TOTAL_RTMEM 64
+
+// STRUCT DEFINITIONS
+struct host {
+	int numPrinters;
+	int numScanners;
+	int numModems;
+	int numDrives;
+	int totalMem;
+
+	int printers[NUM_PRINTERS];
+	int scanners[NUM_SCANNERS];
+	int modems[NUM_MODEMS];
+	int drives[NUM_DRIVES];
+	int memory[TOTAL_MEM];
+
+	int rtMemory;
+	int rtMemoryUsed;
+};
+
+struct pcb {
+	// TODO
+};
+
+struct pcbres {
+	// TODO
+};
 
 // FUNCTION PROTOTYPES
+
+void initSys(void);
 
 #endif
