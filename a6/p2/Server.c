3d4189ffbd7c3e50e83d90a84701c5000310ad82
dmitrons <steven.rezeki@alumni.ubc.ca>
2015-04-10 01:32:17 -0700

    edited youngjin's server code
    make it prints the buffer and fixed order so that it prints "connection
    accepted" at the right order

--- a6/p2/Server.c
+++ a6/p2/Server.c
@@ -8,8 +8,8 @@ int main(int argc , char *argv[])
     WSADATA wsa;
     SOCKET s , new_socket;
     struct sockaddr_in server , client;
-    int c;
-    char *message;
+    int c, recv_size;
+    char *message, client_reply[2000];
  
     printf("\nInitialising Winsock.......");
     if (WSAStartup(MAKEWORD(2,2),&wsa) != 0){ //initialising winsock
@@ -32,9 +32,12 @@ int main(int argc , char *argv[])
      
     //Bind
     if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR) //bind socket
-        printf("Bind failed with error code : %d" , WSAGetLastError()); //prints out if bind fails
-     
-    puts("Bind done!");
+     {
+        printf("Bind failed with error code : %d" , WSAGetLastError());
+        exit(EXIT_FAILURE); // if bind fails, prints error msg and exit
+    }
+	else
+	    puts("Bind done!");
      
  
     //Listen to incoming connections
@@ -44,25 +47,49 @@ int main(int argc , char *argv[])
     puts("Waiting for incoming connections...");
      
     c = sizeof(struct sockaddr_in);
-    new_socket = accept(s , (struct sockaddr *)&client, &c); //accept connection from client
-    if (new_socket == INVALID_SOCKET)
+
+    //new_socket = accept(s , (struct sockaddr *)&client, &c); //accept connection from client
+    while( (new_socket = accept(s , (struct sockaddr *)&client, &c)) != INVALID_SOCKET )
     {
-        printf("accept failed with error code : %d" , WSAGetLastError()); //prints out if client connection fails
+		 puts("Connection accepted");
+         
+        //Reply to the client
+        message = "This is server. Hello Client, I have received your connection\n";
+        send(new_socket , message , strlen(message) , 0);
+		//Receive a reply from the server
+    if((recv_size = recv(new_socket, client_reply , 2000 , 0)) == SOCKET_ERROR)
+    {
+        puts("recv failed");
+    }
+	else
+	{
+		printf("I have receive your message \n");
+		printf("Reply from client : ");
+		client_reply[recv_size] = '\0';
+		puts(client_reply);
+	}
+
+	}
+
+	if (new_socket == INVALID_SOCKET)
+    {
+        printf("accept failed with error code : %d" , WSAGetLastError());
+        return 1;
     }
      
-    puts("Connection accepted");
+   
  
     //Reply to client(test if client can receive or not)
-    message = "Hello Client , I have received your connection. But I have to go now, Bye\n";
+    //message = "Hello Client , I have received your connection. But I have to go now, Bye\n";
     //message = "shorter string"; //test case
-    send(new_socket , message , strlen(message) , 0); //send message to client that it has been connected
+    //send(new_socket , message , strlen(message) , 0); //send message to client that it has been connected
      
-    getchar();
+    //getchar();
  
     //without closesocket keep running client until it runs.
     closesocket(s);
     WSACleanup();
  
-    system("pause");
+    //system("pause");
     return 0;
 }
